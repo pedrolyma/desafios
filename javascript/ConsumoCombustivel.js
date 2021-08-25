fb84cf4276c5ea65c072e9cdaaccbf87336a93b5
pedro lima <pelyma33@gmail.com>
2021-08-25 15:12:18 -0300

    commit de parte dos desafios

---
+++ javascript/ConsumoCombustivel.js
@@ -0,0 +1,7 @@
+// Fundamentos Aritméticos em JavaScript
+// Consumo Médio do Automóvel
+
+let X = parseInt(gets());
+let Y = parseFloat(gets());
+let km = X/Y;
+console.log(km.toFixed(3) +  ' km/l');
---
+++ javascript/NumerosPares.js
@@ -0,0 +1,11 @@
+//Fundamentos Aritméticos em JavaScript
+// Exibindo Números Pares
+
+let N = [gets()];
+
+for ( i = 2; i <= N; i++) {
+  if(i %2===0){
+ console.log(i)
+  }
+
+}
